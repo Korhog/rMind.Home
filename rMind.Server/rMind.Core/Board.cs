9e87b3715bcaf34a5536a4ef9e572cdf8308a2d9
Евгений Клоков <grafinfector@hotmail.com>
2018-06-06 01:20:36 +0300

    Core lib

---
+++ rMind.Server/rMind.Core/Board.cs
@@ -0,0 +1,25 @@
+﻿using Bifrost.Devices.Gpio;
+using Bifrost.Devices.Gpio.Core;
+using System;
+
+namespace rMind.Core
+{
+    public class Board
+    {
+        public static bool Check()
+        {
+            try
+            {
+                var gpioController = GpioController.Instance;
+                var pin = gpioController.OpenPin(21);
+                pin.SetDriveMode(GpioPinDriveMode.Output);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- rMind.Server/rMind.Server/Controllers/ValuesController.cs
+++ rMind.Server/rMind.Server/Controllers/ValuesController.cs
@@ -20,27 +20,12 @@ namespace rMind.Server.Controllers
 
         // GET api/<controller>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public object Get(int id)
         {
-            return "value";
-        }
-
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var result = Core.Board.Check();
+            return new {
+                success = result
+            };
         }
     }
 }
